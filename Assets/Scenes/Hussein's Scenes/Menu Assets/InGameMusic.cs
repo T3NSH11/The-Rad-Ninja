8af1c241c2df2378c076b89c98f8b2de3367bdde
Mamadou <m.hussein@saedubai.com>
2022-07-20 20:07:30 +0400

    Added background music file, implemented it, fixed pause menu issue and made the buttons interactable.

--- Assets/Scenes/Hussein's Scenes/Menu Assets/InGameMusic.cs
+++ Assets/Scenes/Hussein's Scenes/Menu Assets/InGameMusic.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicControl : MonoBehaviour
+public class InGameMusic : MonoBehaviour
 {
-    public static MusicControl instance; // Creates a static varible for a MusicControlScript instance
+    public static InGameMusic instance; // Creates a static varible for a MusicControlScript instance
 
     private void Awake() // Runs before void Start()
     {
-        //DontDestroyOnLoad(this.gameObject); // Don't destroy this gameObject when loading different scenes
+        DontDestroyOnLoad(this.gameObject); // Don't destroy this gameObject when loading different scenes
 
         if (instance == null) // If the MusicControlScript instance variable is null
         {
