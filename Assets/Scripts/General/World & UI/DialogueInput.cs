c909176a22701d823e8c130af77a49289a47af89
myoussef57 <misho.257@hotmail.com>
2022-08-15 19:42:45 +0400

    Added portraits for the dialoghandler
    -got back all my lost changes

--- Assets/Scripts/General/World & UI/DialogueInput.cs
+++ Assets/Scripts/General/World & UI/DialogueInput.cs
@@ -7,6 +7,7 @@ public class DialogueInput : MonoBehaviour
     [SerializeField] string NPCName;
     [SerializeField] string[] dialogue;
     [SerializeField] AudioClip[] dialogueAudio;
+    public GameObject Message;
 
     bool inRange;
 
@@ -23,11 +24,22 @@ public class DialogueInput : MonoBehaviour
     {
         if(other.CompareTag("Cripto"))
             inRange = true;
+
+        if (other.gameObject.tag == "Cripto")
+        {
+            Message.SetActive(true);
+        }
     }
+
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Cripto"))
             inRange = false;
+
+        if (other.gameObject.tag == "Cripto")
+        {
+            Message.SetActive(false);
+        }
     }
 }
