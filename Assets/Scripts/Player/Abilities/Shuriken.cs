7bf2c57aa497db5b9bdca86d71871b03e98109d2
T3NSH11 <m.zaid@student.saedubai.com>
2022-08-24 09:59:06 +0400

    Added new Die animation for enemies hit with shuriken
    - implemented new die animation for enemies

--- Assets/Scripts/Player/Abilities/Shuriken.cs
+++ Assets/Scripts/Player/Abilities/Shuriken.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Shuriken : MonoBehaviour
 {
+    GameObject EnemyObj;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            Destroy(other.gameObject);
+            EnemyObj = other.gameObject;
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Animator>().SetTrigger("Shot");
+            StartCoroutine(Die());
         }
         else if (!other.gameObject.CompareTag("Enemy") && !other.gameObject.CompareTag("Cripto"))
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(EnemyObj);
     }
 }
